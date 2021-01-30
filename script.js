108347d65197eda914033d0df1eca95b86afd82e
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-01-30 11:11:25 +0600

    economy section done and code organized

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getInputValue(isIncrease) {
+function getFirstclassValue(isIncrease) {
     const firstclassInput = document.getElementById('firstclass-input');
     const firstclassNumber = parseInt(firstclassInput.value);
     let firstclassNewCount = 0;
@@ -12,5 +12,21 @@ function getInputValue(isIncrease) {
     firstclassInput.value = firstclassNewCount;
 
     document.getElementById('firstclass-price').innerText = firstclassNewCount * 150;
+}
+
+function getEconomyValue(isIncrease) {
+    const firstclassInput = document.getElementById('economy-input');
+    const firstclassNumber = parseInt(firstclassInput.value);
+    let firstclassNewCount = 0;
+    if (isIncrease == true) {
+        firstclassNewCount = firstclassNumber + 1
+    }
+    if (isIncrease == false && firstclassNumber > 0) {
+        firstclassNewCount = firstclassNumber - 1
+    }
+
+    firstclassInput.value = firstclassNewCount;
+
+    document.getElementById('economy-price').innerText = firstclassNewCount * 100;
 }
 
