4d67effe54b9b7079297198aea44a251dd081dd7
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-01-30 10:55:44 +0600

    organized code firstClass section

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-document.getElementById('firstclass-increase').addEventListener('click', function () {
-    getInputValue(true)
-})
-
-document.getElementById('firstclass-decrease').addEventListener('click', function () {
-    getInputValue(false)
-})
-
 function getInputValue(isIncrease) {
     const firstclassInput = document.getElementById('firstclass-input');
     const firstclassNumber = parseInt(firstclassInput.value);
