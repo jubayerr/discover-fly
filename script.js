a3e232cbe469b05e1af62872817bed8db6ff484f
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-01-30 11:29:17 +0600

    organized code of both section

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function getFirstclassValue(isIncrease) {
-    const firstclassInput = document.getElementById('firstclass-input');
+function getTicketValue(product, isIncrease) {
+    const firstclassInput = document.getElementById(product + '-input');
     const firstclassNumber = parseInt(firstclassInput.value);
     let firstclassNewCount = 0;
     if (isIncrease == true) {
@@ -10,23 +10,16 @@ function getFirstclassValue(isIncrease) {
     }
 
     firstclassInput.value = firstclassNewCount;
-
-    document.getElementById('firstclass-price').innerText = firstclassNewCount * 150;
-}
-
-function getEconomyValue(isIncrease) {
-    const firstclassInput = document.getElementById('economy-input');
-    const firstclassNumber = parseInt(firstclassInput.value);
-    let firstclassNewCount = 0;
-    if (isIncrease == true) {
-        firstclassNewCount = firstclassNumber + 1
+    let firstclassPriceTotal = 0;
+    if (product == 'firstclass') {
+        firstclassPriceTotal = firstclassNewCount * 150;
     }
-    if (isIncrease == false && firstclassNumber > 0) {
-        firstclassNewCount = firstclassNumber - 1
+    if (product == 'economy') {
+        firstclassPriceTotal = firstclassNewCount * 100;
     }
 
-    firstclassInput.value = firstclassNewCount;
 
-    document.getElementById('economy-price').innerText = firstclassNewCount * 100;
+    document.getElementById(product + '-price').innerText = "$" + firstclassPriceTotal;
 }
+
 
